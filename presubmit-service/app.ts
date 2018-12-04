55d5ab96118fc979a4bb9bc72aceca3522612d5d
Joey Perrott <josephperrott.github@gmail.com>
2018-12-04 14:24:22 -0800

    Create presubmit-service module (#1)

---
+++ presubmit-service/app.ts
@@ -0,0 +1,53 @@
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+
+import {getCherryPickConflictStatus} from './check-pr';
+
+/** The body of the request for POST requests to the /check_pr handler. */
+interface CheckPrRequest {
+  pr: string;
+  repo: string;
+  org: string;
+  branch: string;
+}
+
+/** The port to listen for HTTP requests on. */
+const PORT = process.env.PORT || 8080;
+/** The express application instance. */
+const httpServer = express();
+
+// Since POST requests send all data in the body, it needs to be parsed as a
+// JSON object to be easily accessed.
+httpServer.use(bodyParser.json());
+
+// Handle POST requests at /check_pr.
+httpServer.post('/check_pr', (request: express.Request, response: express.Response) => {
+  const options = request.body as CheckPrRequest;
+  const {pr, repo, org, branch} = options;
+
+  // All responses should be JSON responses.
+  response.setHeader('content-type', 'application/json');
+  // Using the Cache-Control header, prevent caching of requests.
+  response.setHeader('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+
+  // Check if all parameters are provided.
+  if (!pr || !repo || !branch || !org) {
+    response.status(400);
+    response.send({status: 'missing_params', message: `Missing required parameter(s)`});
+    return;
+  }
+  
+  response.send(getCherryPickConflictStatus(org, repo, pr, branch));
+});
+
+// Handle all GET requests.
+httpServer.get('/*', (req: express.Request, res: express.Response) => {
+  res.status(200);
+  res.send('Requests to the presubmit-service module must be made via POST request.');
+});
+
+// Start listening on the port for requests.
+httpServer.listen(PORT, () => {
+  console.log(`App initialized.`);
+  console.log(`Listening on port: ${PORT}`);
+});
---
+++ presubmit-service/check-pr.ts
@@ -0,0 +1,14 @@
+import * as shell from 'shelljs';
+
+/** Determine if the PR can apply to a repo's targeted branch. */
+export const getCherryPickConflictStatus = 
+    (org: string, repo: string, pr: string, branch: string) => {
+  // TODO(josephperrott): Use stderr/stdout output from the script to fill in the message property
+  // values in the returned object.
+  const prCheck = shell.exec(`./check_if_pr_can_cherry_pick.sh ${org} ${repo} ${pr} ${branch}`);
+  if (prCheck.code === 0) {
+    return {status: 'success', message: ''};
+  } else {
+    return {status: 'failure', message: ''};
+  }
+}
