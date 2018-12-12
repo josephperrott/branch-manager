e7510baf9ae09f79185451086652a95714e2bcb0
Joey Perrott <josephperrott.github@gmail.com>
2018-12-12 11:36:23 -0800

    Move task-service to use firebase functions instead of cloud tasks (#8)

---
+++ task-service/presubmit-pr.ts
@@ -0,0 +1,36 @@
+import * as fetch from 'node-fetch';
+import * as functions from 'firebase-functions';
+
+import {setStatusOnGithub} from '../shared/github';
+
+/** 
+ * The handler function for a presubmit pr tasks triggered by messages on the 
+ * 'presubmit-pr-task' pubsub topic.
+ * 
+ * Checks the if the pr is able to be cherry-picked cleanly to its target branch, and
+ * set the status on github as a result.
+ */
+export const handlePresubmitPrTask = functions
+  .pubsub
+  .topic('presubmit-pr-task')
+  .onPublish(async message => {
+    const {org, repo, pr, branch, sha} = message.json;
+    const url = `https://presubmit-service-dot-branch-manager.appspot.com/check_pr`;
+    const config = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({org, repo, pr, branch, sha})
+    }
+    try {
+      const response = await fetch(url, config);
+      await setStatusOnGithub(org, repo, sha, response.json().status)
+
+    } catch (e) {
+      await setStatusOnGithub(
+        org, repo, sha, 'failure', 
+        'The PR does not cleanly cherry-pick onto the targetted branch');
+      
+    }
+  });
