dcedfe809c6edfe8790fc78fa64e3bfe9fb2606b
Joey Perrott <josephperrott.github@gmail.com>
2018-12-13 10:06:14 -0800

    change names/response types of some firestore util functions

--- shared/firestore-models/util.ts
+++ shared/firestore-models/util.ts
@@ -1,37 +1,17 @@
 import * as firebase from 'firebase-admin';
 
-import {getConfig, BranchManagerRepoConfig} from './repo-config'
+import {getConfig, BranchManagerRepoConfig, BranchManagerRepoConfigBranch} from './repo-config'
 import {BranchManagerPullRequest} from './pull-request'
 
-/** Determine when a label is one of the target labels based on the repos config. */
-export async function isConfiguredTargetLabel(configId: string, label: string): Promise<boolean> {
-  const configDoc = await getConfig(configId);
-  if (configDoc.exists) {
-    const config = configDoc.data() as BranchManagerRepoConfig;
-    return !!config.branches.find(branch => branch.label === label);
-  }
-  return false;
-}
-
 /** Retrieve the target branch for a pull request based on the repos config. */
-export async function getTargetBranch(
-    configId: string, pullRequestRef: firebase.firestore.DocumentReference): Promise<string> {
+export async function getBranchForPullRequest(
+    configId: string, pullRequestRef: firebase.firestore.DocumentReference):
+    Promise<BranchManagerRepoConfigBranch> {
   const configDoc = await getConfig(configId);
   const pullRequestDoc = await pullRequestRef.get();
-  let target = {
-    branch: '',
-    label: ''
-  }
   if (configDoc.exists && pullRequestDoc.exists) {
     const config = configDoc.data() as BranchManagerRepoConfig;
     const pullRequest = pullRequestDoc.data() as BranchManagerPullRequest;
-    target = config.branches.find(branch => pullRequest.labels.includes(branch.label)) || target;
+    return config.branches.find(branch => pullRequest.labels.includes(branch.label));
   }
-  return target.branch;
-}
-
-/** Determine if a pull request as one of the targeted labels based on the repos config. */
-export async function hasTargetedLabel(
-    configId: string, pullRequestRef: firebase.firestore.DocumentReference): Promise<boolean> {
-  return !!(await getTargetBranch(configId, pullRequestRef));
 }
